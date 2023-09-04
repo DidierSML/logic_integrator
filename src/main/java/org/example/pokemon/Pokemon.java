fdd1419dd32273f34f7bb9a9684d114c4128787b
DidierSML <didiersmaldonad@gmail.com>
2023-09-04 15:42:05 -0500

    "Creating Abstract Class named (Pokemon)"

--- src/main/java/org/example/pokemon/Pokemon.java
+++ src/main/java/org/example/pokemon/Pokemon.java
@@ -1,4 +1,23 @@
 package org.example.pokemon;
 
-public class Pokemon {
+public abstract class Pokemon {
+
+    //Atributos para el uso de todas sus Clases hijas
+    protected int numPokedex;
+    protected String nombre;
+    protected int peso;
+    protected String sexo;
+    protected int temporada;
+
+    //Metodos abstractos que podran usar todas sus Clases hijas
+    protected abstract void atacarPlacaje();
+    protected abstract void atacarAraniazo();
+    protected abstract void atacarMordisco();
+
 }
+
+/**
+ * Clase Abstracta (Pokemon), que servirá de plantilla para otras Clases que al extender de
+ * esta podrán usar sus atributos y métodos.
+ * Clases que extenderán de (Pokemon) : (Bulbasor, Charmander, Pikachu, Squirtle)
+ */
