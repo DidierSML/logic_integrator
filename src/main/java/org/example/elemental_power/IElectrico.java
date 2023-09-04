cb09ab23b991466c75a0ace3f850f1b03dffef0a
DidierSML <didiersmaldonad@gmail.com>
2023-09-04 15:58:09 -0500

    "Creating (I) (elemental_power) for Pokemons"

--- src/main/java/org/example/elemental_power/IElectrico.java
+++ src/main/java/org/example/elemental_power/IElectrico.java
@@ -1,4 +1,14 @@
 package org.example.elemental_power;
 
 public interface IElectrico {
+
+    void atacarImpactTrueno();
+    void atacarPunioTrueno();
 }
+
+/**
+ * (I) de Tipo Eléctrico: esta será implementada por aquellas Clases que
+ * determinene usar todos los métodos abstractos que esta contiene; es decir,
+ * que Cada Clase que la implemente esta (I) podrá decidir como usar los métodos.
+ * Clases que implementaran esta interfaz: (Pikachu)
+ */
--- src/main/java/org/example/elemental_power/IPlanta.java
+++ src/main/java/org/example/elemental_power/IPlanta.java
@@ -1,4 +1,14 @@
 package org.example.elemental_power;
 
 public interface IPlanta {
+
+    void atacarDrenaje();
+    void atacarParalizar();
 }
+
+/**
+ * (I) de Tipo Planta: esta será implementada por aquellas Clases que
+ * determinene usar todos los métodos abstractos que esta contiene; es decir,
+ * que Cada Clase que la implemente esta (I) podrá decidir como usar los métodos.
+ * Clases que implementaran esta interfaz: (Bulbasor)
+ */
